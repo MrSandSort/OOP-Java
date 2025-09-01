24260ff7b7095d8a421eb98a77b97ab96564dd87
MrSandSOrt <paudelsandesh1200@gmail.com>
2025-09-01 08:58:11 +0545

    added string related practice functions

---
+++ Practice/String/stringTitle.java
@@ -0,0 +1,25 @@
+package Practice;
+
+public class stringTitle {
+    public static void main(String[] args) {
+        String str = "hello world from java";
+        String titledStr = toTitleCase(str);
+        System.out.println("Original String: " + str);
+        System.out.println("Title Case String: " + titledStr);
+    }
+
+    public static String toTitleCase(String str) {
+        String[] words = str.split(" ");
+        StringBuilder titled = new StringBuilder();
+
+        for (String word : words) {
+            if (word.length() > 0) {
+                titled.append(Character.toUpperCase(word.charAt(0)))
+                      .append(word.substring(1).toLowerCase())
+                      .append(" ");
+            }
+        }
+
+        return titled.toString().trim();
+    }
+}
---
+++ Typecasting/explicit_typecasting.java
@@ -0,0 +1,12 @@
+package Typecasting;
+
+class Explicit {
+
+    public static void main(String args[]){
+        double myDouble=9.78;
+        int myInt= (int) myDouble; // manual casting: double to int
+        System.out.println(myDouble);   // Outputs 9.78
+        System.out.println(myInt);      // Outputs 9
+    }
+    
+}
